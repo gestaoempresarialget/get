3438f8d21a7837172595e0835ab371be95d50d82
gestaoempresarialget <gestaoempresarialget@gmail.com>
2026-09-25 13:39:17 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
+'use client'
 
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>GET - Gestão Financeira</h1>
+      <p>O sistema está a funcionar corretamente!</p>
+    </main>
+  )
+}
